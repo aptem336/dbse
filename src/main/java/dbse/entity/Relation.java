7e413917ae342dc736a53db4509e5736823b61a6
Artem Popov <aptem336@gmail.com>
2019-11-20 17:08:47 +0800

    начинаю добавлять функциональность редактора схем

--- src/main/java/dbse/entity/Relation.java
+++ src/main/java/dbse/entity/Relation.java
@@ -9,11 +9,11 @@ import java.util.List;
 @NamedQuery(name = RelationService.getAllNamedQueryName, query = "SELECT r FROM Relation r")
 public class Relation extends AbstractEntity {
 
-    public void addAttribute() {
-        Attribute attribute = new Attribute();
-        attribute.setRelation(this);
-        attributeList.add(attribute);
-    }
+//    public void addAttribute() {
+//        Attribute attribute = new Attribute();
+//        attribute.setRelation(this);
+//        attributeList.add(attribute);
+//    }
 
     private String name;
     @OneToMany(mappedBy = "relation", cascade = CascadeType.ALL)
