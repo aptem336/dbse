b4101bf3bc30c35a83fca1fa7de485484f38c3c3
Artem Popov <aptem336@gmail.com>
2020-04-07 19:00:09 +0800

    удалил излюшнюю типизацию

--- src/main/java/view/draggable/DragMethod.java
+++
@@ -1,4 +0,0 @@
-package view.draggable;
-
-public interface DragMethod {
-}
--- src/main/java/view/draggable/PositionedDragMethod.java
+++ src/main/java/view/draggable/PositionedDragMethod.java
@@ -3,7 +3,7 @@ package view.draggable;
 import model.Model;
 import view.Positioned;
 
-public class PositionedDragMethod implements DragMethod {
-    public static<T extends Model & DragSource & Positioned> void accept(T t) {
+public class PositionedDragMethod {
+    public static<T extends Model & DragSource<T> & Positioned> void accept(T t) {
     }
 }
